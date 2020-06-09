6389686022946bf558e75ff92b5845faee9df16c
pragya2211 <44156976+pragya2211@users.noreply.github.com>
2020-06-10 05:14:43 +0530

    update

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php include ('main-header.php'); ?>
 <!--Concatenate with the particular page HTML followed by the FOOTER HTML-->
 <!--Append to the common top HTML-->
+
+<style type="text/css">
+
+	form input: hover
+	{
+		background: gray;
+	}
+	form input[value]
+	{
+		color: white;
+		font-variant: small-caps;
+
+	}
+	input[type=submit] {
+	  width: 15%;
+	  font-size: 15px;
+	  background-color: #3fada8;
+	  color: white;
+	  padding: 14px 20px;
+	  margin: 8px 0;
+	  border: none;
+	  border-radius: 4px;
+	  cursor: pointer;
+}
+</style>
 <div class="page">
 <!--Concatenate with the particular page HTML followed by the FOOTER HTML-->
 <!--<div><img src="./images/ssh-banner.jpg" title="SSH Department IIITD"/></div> -->
@@ -14,6 +39,12 @@ writing lab within the SSH department. It will primarily provide writing assista
 divisions, schools, and academic and administrative programs.        </p>
         <p><a href="./awl-about.php">Read More...</a></p>
 
+<h3>If you want to book a session for consultation with Writing Lab, please click here:
+<form action="form.php">
+        <input type="submit" name="submit" value="Book a slot">
+
+      </form>
+  </h3>
     
 <div id="cards-container">
 
@@ -27,7 +58,7 @@ divisions, schools, and academic and administrative programs.        </p>
 </div>
 <div class="card rteleft facultycard" style="height:300px; width:400px;">
 <p style="float:center; margin-left: 20px;"><strong><img alt="" src="./images/Workshop.jpeg" style="float:right;height:220px; width: 350px; border:1px solid; padding:2px;"></strong></p>
-<p align="left" style="width:400px; font-family:&#39;Old Standard TT&#39;;font-size:20px;color:#3fada8;margin-left: 120px;margin-top: 255px"><a href="Workshop.php" style="color: #3fada8">Workshops</a></p>
+<p align="left" style="width:400px; font-family:&#39;Old Standard TT&#39;;font-size:20px;color:#3fada8;margin-left: 120px;margin-top: 255px"><a href="http://localhost:8000/Workshop.php" style="color: #3fada8">Workshops</a></p>
 </div>
 <div class="card rteleft facultycard" style="height:300px; width:400px;">
 <p style="float:center; margin-left: 20px;"><strong><img alt="" src="./images/outreach.jpg" style="float:right;height:220px; width: 350px; border:1px solid; padding:2px;"></strong></p>
