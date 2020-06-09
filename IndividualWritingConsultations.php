9e3791a9e16997f9ac6bbf538e6a60e414e67762
pragya2211 <44156976+pragya2211@users.noreply.github.com>
2020-06-10 05:15:23 +0530

    update

--- IndividualWritingConsultations.php
+++ IndividualWritingConsultations.php
@@ -1,49 +1,60 @@
 <?php include ('main-header.php'); ?>
+<style type="text/css">
+
+	form input: hover
+	{
+		background: gray;
+	}
+	form input[value]
+	{
+		color: white;
+		font-variant: small-caps;
+
+	}
+	input[type=submit] {
+	  width: 20%;
+	  font-size: 17px;
+	  background-color: #3fada8;
+	  color: white;
+	  padding: 14px 20px;
+	  margin: 8px 0;
+	  border: none;
+	  border-radius: 4px;
+	  cursor: pointer;
+}
+
+</style>
 <div class="page">
 
 <h1>Individual Writing Consultations</h1>
 <p align="justify" style="font-size:14px;">If you are writing a paper and you need some help to improve it, structure it properly, and
 have objective and constructive analysis of your work, you can definitely request for a
 formal appointment for a Individual Writing Consultation. </p>
-<h3> Slots:</h3>
-<p style="font-sixe:14px;" align="justify">
-3 - 4 p.m<br>
+<h2> Slots:</h2>
+<p style="font-size:16px;" align="justify">
+Every Wednesday, 3 - 4 p.m<br>
 Each slot duration: 20 minutes
-<br>
+</p>
+<p style="font-size:14px;" align="justify">
 Please check the FAQs carefully before you put in a request.</p>
+<br>
+<h2> Click here to book a slot </h2>
+<form action="form.php">
+        <input type="submit" name="submit" value="Book a slot">
+
+      </form>
+
+<h2> To view the FAQs, please click here</h2>
+<form action="faq.php"> 
+
+          
+        <input type="submit" name="button2"
+                 value="FAQs" />
+
+    </form> 
 
 
-<h2><u> FAQs:</u></h2>
-<h3>Can I seek individual writing consultation?</h3>
-<p align="justify" style="font-size:14px;">If you are writing your research paper and you need some help in it, you might feel free to
-request for a slot on Individual writing consultations. We will arrange for a one-on-one
-meeting. </p>
 
-<h3>What do I have to bring if I may an appointment for individual writing consultation?</h3>
-<p align="justify" style="font-size:14px;">It will be always better if you bring the draft you are currently working on. Please do not
-just come with a basic outline of notes or without anything. </p>
-
-<h3>Why types of writing do we not handle?</h3>
-<p align="justify" style="font-size:14px;">If you need help with your resume/cv, and other non-academic career materials, regular
-course assignments, AWL won’t be doing that. </p>
-
-<h3>Where do individual guidance/consultations take place?</h3>
-<p align="justify" style="font-size:14px;">In the Writing Lab itself, A-219. </p>
-
-<h3>Do you offer online consultations?</h3>
-<p align="justify" style="font-size:14px;">Yes, if necessary. </p>
-
-<h3>How do I cancel a meeting?</h3>
-<p align="justify" style="font-size:14px;">Just drop an email if you need to cancel your consultation
-an hour earlier.<br>
-Kindly refrain from unexplained absences at appointments or frivolous cancelations. </p>
-
-<h3>Does the AWL provide copy-editing or proofreading services?</h3>
-<p align="justify" style="font-size:14px;">No, the AWL does not provide copy-editing or proofreading services. The Lab’s objective is
-to help graduate students grow as independent academic writers, rather than proofread for
-
-them. If you are interested in improving your editing and proofreading skills, please seek
-help somewhere else. </p>
 
 <?php include ('main-footer.php'); ?>
 	
