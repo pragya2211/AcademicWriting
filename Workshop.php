c6173af55a06a881c67980d9b3eda07642820267
pragya2211 <44156976+pragya2211@users.noreply.github.com>
2020-06-15 02:19:24 +0530

    Update Workshop.php

--- Workshop.php
+++ Workshop.php
@@ -19,6 +19,24 @@ for April end, but now not taking place because of COVID-19)</li>
 
  </p>
 
+ <h1>Sessions</h1>
+<p align="justify" style="font-size:16px;"><b>Sessions for in-house skill building by Dr Payel C Mukherjee, Coordinator, AWL and
+Visiting Faculty, SSH
+</b>
+</p>
+<p align="justify" style="font-size:14px;">
+<ul>
+	<li>Public Speaking Skills; 27 September 2019</li>
+	<li>Effective Skills for Engineers and Scientists (with Central Library IIIT Delhi); 30
+October 2019</li>
+<li>Communicating your science for public outreach; 13 November 2019</li>
+<li>3-Day module on Advanced Writing; 28 - 31December 2019</li>
+<li>Ethics in Research and Publications (with Central Library IIIT Delhi) ; 22
+January 2020</li>
+</ul>
+
+ </p>
+
 
 <?php include ('main-footer.php'); ?>
 	
