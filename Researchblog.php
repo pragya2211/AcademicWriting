135eefd1eeaac4b22d7c44fe382ca4b3baf3ce8c
pragya2211 <44156976+pragya2211@users.noreply.github.com>
2020-06-15 02:09:46 +0530

    Update Researchblog.php

--- Researchblog.php
+++ Researchblog.php
@@ -1,6 +1,72 @@
 <?php include ('main-header.php'); ?>
 <div class="page">
-<h2>Content Under Revision</h2>
+
+
+<div id="cards-container">
+
+<div class="card rteleft facultycard" style="height:550px; width:350px;">
+<p style="float:center;"><strong><img alt="" src="./images/IndividualWritingConsultations.jpg" style="float:center;height:200px; width: 250px; border:1px solid; padding:2px;"></strong></p>
+<p align="left" style="width:350px; font-family:Old Standard TT;font-size:17px;margin-left: 15px; ">
+	<a style="color: #3fada8">  Topic </a>:<br> Is Game-Based Learning always beneficial?
+	<br><br>
+	<a style="color: #3fada8">Article type </a>: <br>Argumentative Opinion Piece
+	<br><br>
+	<a style="color: #3fada8">Name</a> :<br> Nakul Gupta, CSE Undergraduate,  IIIT - Delhi
+	<br><br>
+	<a style="color: #3fada8">Course name</a> : <br>SSH311 Advanced Writing
+	<br><br>
+	<a style="color: #3fada8">To view the paper, click here</a>,  
+	<a href = "https://www.youtube.com/watch?v=LDH6xg-55OQ&t=1s"> Link </a>
+</p>
+</div>
+<div class="card rteleft facultycard" style="height:550px; width:350px;">
+<p style="float:center;"><strong><img alt="" src="./images/IndividualWritingConsultations.jpg" style="float:center;height:200px; width: 250px; border:1px solid; padding:2px;"></strong></p>
+<p align="left" style="width:350px;margin-left: 15px;  font-family:Old Standard TT;font-size:17px;">
+	<a style="color: #3fada8">Topic </a>:<br>Women and the partition of 1947
+	<br><br>
+	<a style="color: #3fada8">Video type </a>: <br>Argumentative Opinion Piece
+	<br><br>
+
+	<a style="color: #3fada8">Name</a> :<br> Dhairya Chaudhary, Pritish Wadhwa, Aryan Taneja, Rahul Sethi, Samridh Shakya 
+	<br><br>
+	<a style="color: #3fada8">Course name</a> :<br> 
+	<br><br>
+	<a style="color: #3fada8">To view the video, click here</a>,  
+	<a href = "https://www.youtube.com/watch?v=LDH6xg-55OQ&t=1s"> Link </a>
+</p>
+</div>
+<div class="card rteleft facultycard" style="height:550px; width:350px;">
+<p style="float:center;"><strong><img alt="" src="./images/IndividualWritingConsultations.jpg" style="float:center;height:200px; width: 250px; border:1px solid; padding:2px;"></strong></p>
+<p align="left" style="width:350px;margin-left: 15px; font-family:Old Standard TT;font-size:17px;">
+	<a style="color: #3fada8">Topic </a>:<br>Is the MIT Moral Machine Experiment the Best Solution to The
+Ethical Problems Associated with Autonomous Cars?
+	<br><br>
+	<a style="color: #3fada8">Article type </a>: <br>Argumentative Opinion Piece
+	<br><br>
+	<a style="color: #3fada8">Name</a> :<br> Dewangee Agarwal, CSE Undergraduate,  IIIT - Delhi
+	<br><br>
+	<a style="color: #3fada8">Course name</a> : <br>SSH311 Advanced Writing
+	<br><br>
+	<a style="color: #3fada8">To view the paper, click here</a>,  
+	<a href = "https://drive.google.com/open?id=1tOis4jMpjspJ2EWnbbW36fLPT4cQTs3u"> Link </a>
+</p>
+</div>
+<div class="card rteleft facultycard" style="height:550px; width:350px;">
+<p style="float:center;"><strong><img alt="" src="./images/IndividualWritingConsultations.jpg" style="float:center;height:200px; width: 250px; border:1px solid; padding:2px;"></strong></p>
+<p align="left" style="width:350px;margin-left: 15px; font-family:Old Standard TT;font-size:17px;">
+	<a style="color: #3fada8">Topic </a>:<br>Probabilistic Spin Logic: A Review
+	<br><br>
+	<a style="color: #3fada8">Article type </a>: <br>Argumentative Opinion Piece
+	<br><br>
+	<a style="color: #3fada8">Name</a> :<br> Amina Haroon, ECE Undergraduate,  IIIT - Delhi
+	<br><br>
+	<a style="color: #3fada8">Course name</a> : <br>SSH311 Advanced Writing
+	<br><br>
+	<a style="color: #3fada8">To view the paper, click here</a>,  
+	<a href = "https://drive.google.com/open?id=1VvpTWE3Usr24Rf0TdI-O22YLsxa-SAEl"> Link </a>
+</p>
+</div>
+</div> 
 
 
 <?php include ('main-footer.php'); ?>
