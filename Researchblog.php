09ba5d75a913fbde87902021387fc5d2def9fcbf
pragya2211 <44156976+pragya2211@users.noreply.github.com>
2020-06-18 17:56:18 +0530

    Update Researchblog.php

--- Researchblog.php
+++ Researchblog.php
@@ -4,60 +4,58 @@
 
 <div id="cards-container">
 
-<div class="card rteleft facultycard" style="height:400px; width:350px;">
-<p align="left" style="width:350px; font-family:Old Standard TT;font-size:17px;margin-left: 15px; ">
-	<a style="color: #3fada8"> Is Game-Based Learning always beneficial? </a>
+<div class="card rteleft facultycard" style="height:300px; width:250px;">
+<p align="left" style=" font-family:Old Standard TT;font-size:17px;margin-left: 15px; ">
+	<a style="color: #3fada8;font-size: 23px; width: 100%"> Is Game-Based Learning always beneficial? </a>
 	<br><br><br>
-	<a style="color: #3fada8">Article type </a>: <br><br>Argumentative Opinion Piece
-	<br><br><br>
-	<a style="color: #3fada8">Name</a> :<br><br> Nakul Gupta, CSE Undergraduate,  IIIT - Delhi
-	<br><br><br>
-	<a style="color: #3fada8">Course name</a> : <br><br>SSH311 Advanced Writing
-	<br><br><br>
+	<a style="color: #3fada8">Article type </a>: Argumentative Opinion Piece
+	<br><br>
+	<a style="color: #3fada8">Name</a> : Nakul Gupta, CSE Undergraduate,  IIIT - Delhi
+	<br><br>
+	<a style="color: #3fada8">Course name</a> : SSH311 Advanced Writing
+	<br><br>
 	<a style="color: #3fada8">To view the paper, click here</a>,  
 	<a href = "paper1.php"> Link </a>
 </p>
 </div>
-<div class="card rteleft facultycard" style="height:400px; width:350px;">
-<p align="left" style="width:350px;margin-left: 15px;  font-family:Old Standard TT;font-size:17px;">
-	<a style="color: #3fada8">Topic </a>:<br><br>Women and the partition of 1947
+<div class="card rteleft facultycard" style="height:300px; width:250px;">
+	<p align="left" style=" font-family:Old Standard TT;font-size:17px;margin-left: 15px; ">
+	<a style="color: #3fada8;font-size: 23px; width: 100%">Women and the partition of 1947 </a>
 	<br><br><br>
-	<a style="color: #3fada8">Video type </a>: <br><br>Narrative on the women and partition
-	<br><br><br>
-
-	<a style="color: #3fada8">Name</a> :<br> <br>Dhairya Chaudhary, Pritish Wadhwa, Aryan Taneja, Rahul Sethi, Samridh Shakya 
-	<br><br><br>
-	<a style="color: #3fada8">Course name</a> :<br> <br>SSH215 Nation and her Narratives
-	<br><br><br>
+	<a style="color: #3fada8">Video type </a>: Narrative on the women and partition
+	<br><br>
+	<a style="color: #3fada8">Name</a> : Dhairya Chaudhary, Pritish Wadhwa, Aryan Taneja, Rahul Sethi, Samridh Shakya
+	<br><br>
+	<a style="color: #3fada8">Course name</a> : SSH215 Nation and her Narratives
+	<br><br>
 	<a style="color: #3fada8">To view the video, click here</a>,  
-	<a href = "https://www.youtube.com/watch?v=LDH6xg-55OQ&t=1s"> Link </a>
-</p>
+	<a href = "https://www.youtube.com/watch?v=LDH6xg-55OQ&t=1s"> Link </a></p>
 </div>
-<div class="card rteleft facultycard" style="height:420px; width:350px;">
-<p align="left" style="width:350px;margin-left: 15px; font-family:Old Standard TT;font-size:17px;">
-	<a style="color: #3fada8">Topic </a>:<br><br>Is the MIT Moral Machine Experiment the Best Solution to The
-Ethical Problems Associated with Autonomous Cars?
+<div class="card rteleft facultycard" style="height:320px; width:250px;">
+	<p align="left" style=" font-family:Old Standard TT;font-size:17px;margin-left: 15px; ">
+	<a style="color: #3fada8;font-size: 20px; width: 100%"> Is the MIT Moral Machine Experiment the Best Solution to The
+Ethical Problems Associated with Autonomous Cars? </a>
 	<br><br><br>
-	<a style="color: #3fada8">Article type </a>: <br><br>Argumentative Opinion Piece
-	<br><br><br>
-	<a style="color: #3fada8">Name</a> :<br><br> Dewangee Agarwal, CSE Undergraduate,  IIIT - Delhi
-	<br><br><br>
-	<a style="color: #3fada8">Course name</a> : <br><br>SSH311 Advanced Writing
-	<br><br><br>
+	<a style="color: #3fada8">Article type </a>: Argumentative Opinion Piece
+	<br><br>
+	<a style="color: #3fada8">Name</a> : Dewangee Agarwal, CSE Undergraduate,  IIIT - Delhi
+	<br><br>
+	<a style="color: #3fada8">Course name</a> : SSH311 Advanced Writing
+	<br><br>
 	<a style="color: #3fada8">To view the paper, click here</a>,  
 	<a href = "paper2.php"> Link </a>
 </p>
 </div>
-<div class="card rteleft facultycard" style="height:420px; width:350px;">
-<p align="left" style="width:350px;margin-left: 15px; font-family:Old Standard TT;font-size:17px;">
-	<a style="color: #3fada8">Topic </a>:<br><br>Probabilistic Spin Logic: A Review
+<div class="card rteleft facultycard" style="height:300px; width:250px;">
+	<p align="left" style=" font-family:Old Standard TT;font-size:17px;margin-left: 15px; ">
+	<a style="color: #3fada8;font-size: 23px; width: 100%"> Probabilistic Spin Logic: A Review </a>
 	<br><br><br>
-	<a style="color: #3fada8">Article type </a>:<br> <br>Argumentative Opinion Piece
-	<br><br><br>
-	<a style="color: #3fada8">Name</a> :<br><br> Amina Haroon, ECE Undergraduate,  IIIT - Delhi
-	<br><br><br>
-	<a style="color: #3fada8">Course name</a> : <br><br>SSH311 Advanced Writing
-	<br><br><br>
+	<a style="color: #3fada8">Article type </a>: Argumentative Opinion Piece
+	<br><br>
+	<a style="color: #3fada8">Name</a> : Amina Haroon, ECE Undergraduate,  IIIT - Delhi
+	<br><br>
+	<a style="color: #3fada8">Course name</a> : SSH311 Advanced Writing
+	<br><br>
 	<a style="color: #3fada8">To view the paper, click here</a>,  
 	<a href = "https://drive.google.com/open?id=1VvpTWE3Usr24Rf0TdI-O22YLsxa-SAEl"> Link </a>
 </p>
